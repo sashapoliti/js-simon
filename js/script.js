57feb641c7e24110325021494ff161223f69e3ed
sashapoliti <sas.politi@gmail.com>
2024-03-20 14:51:39 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,5 @@
+/* Visualizzare in pagina 5 numeri casuali ( tra 1 e 100) non duplicati.
+Da lì parte un timer di 30 secondi.
+Dopo i 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt() ( o meglio caselle di input).
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
+
